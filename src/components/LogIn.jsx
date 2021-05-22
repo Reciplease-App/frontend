b372e3496d386f89463bfbabeb2372db3b047f78
Austin Greer <agreer.ag12@gmail.com>
2021-05-22 11:49:58 -0400

    email and password are now being controlled by state. Also installed styled-components

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
+
 
 function LogIn() {
+    // initial form values
+    const formValues = {
+        email: '',
+        password: ''
+    }
+
+    const formErrors = {
+        email: '',
+        password: ''
+    }
+
+    // tracking form values with useState
+    const [loginValues, setLoginValues] = useState(formValues)
+    const [loginErrors, setLoginErrors] = useState(formErrors)
+
+    const inputValue = (e) => {
+        setLoginValues({...loginValues, [e.target.name]: e.target.value})
+    }
+    
     return (
         <div>
             <h1>Login</h1>
@@ -9,13 +29,17 @@ function LogIn() {
             <form>
                 <input 
                     type='text'
-                    name='Email'
+                    name='email'
                     placeholder='Email'
+                    value={loginValues.email}
+                    onChange={inputValue}
                 />
                 <input 
                     type='password'
-                    name='Password'
+                    name='password'
                     placeholder='Password'
+                    value={loginValues.password}
+                    onChange={inputValue}
                 />
                 <button>Let's get cook'n</button>
                 <p>Sign Up or Learn More</p>
