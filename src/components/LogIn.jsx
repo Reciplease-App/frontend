3c0a9848e201192567291b8459e2ee0eccada206
Austin Greer <agreer.ag12@gmail.com>
2021-05-22 11:37:46 -0400

    setup basic scaffolding of login form

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -3,7 +3,23 @@ import React from 'react'
 function LogIn() {
     return (
         <div>
-            Hello From the Login component
+            <h1>Login</h1>
+            <p>Welcome back! Lettuce show you some more recipes to fall in love with!</p>
+
+            <form>
+                <input 
+                    type='text'
+                    name='Email'
+                    placeholder='Email'
+                />
+                <input 
+                    type='password'
+                    name='Password'
+                    placeholder='Password'
+                />
+                <button>Let's get cook'n</button>
+                <p>Sign Up or Learn More</p>
+            </form>
         </div>
     )
 }
