768ac08172f4d0bc59322391075f66970326d380
unknown <agreer.ag12@gmail.com>
2021-07-31 22:08:27 -0400

    completed styling for login modal utilizing material ui components and styled components

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import * as Yup from 'yup';
+import { Button, TextField } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 import loginSchema from '../formSchema/loginSchema';
 
@@ -18,21 +20,8 @@ function LogIn() {
 
     const [loginValues, setLoginValues] = useState(formValues);
     const [loginErrors, setLoginErrors] = useState(formErrors);
-    // const [disabled, setDisabled] = useState(true);
 
-
-
-    //useEffect is checking if the form input is valid. If so, then it is enabling the submit button
-    // useEffect(() => {
-    //     loginSchema.isValid(loginValues)
-    //         .then(valid => setDisabled(!valid))
-
-    // }, [loginValues])
-
-
-    // handler function - tracking errors
     const handleFormErrors = (name, value) => {
-    
         Yup.reach(loginSchema,name).validate(value)
             .then(valid => {
                 setLoginErrors({...loginErrors, [name]: ''})
@@ -62,22 +51,26 @@ function LogIn() {
                 {formErrors.email && <p>{formErrors.email}</p>}
                 {formErrors.password && <p>{formErrors.password}</p>}
                 <form onSubmit={handleSubmit}>
-                    <input 
+                    <TextField
+                        variant="outlined"
                         type='text'
                         name='email'
                         placeholder='Email'
                         value={loginValues.email}
                         onChange={handleChange}
                     />
-                    <input 
+                    <TextField
+                        variant="outlined"
                         type='password'
                         name='password'
                         placeholder='Password'
                         value={loginValues.password}
                         onChange={handleChange}
                     />
-                    <button>Let's get cook'n</button>
-                    <p>Sign Up or Learn More</p>
+                    <Button variant="contained">Let's get cook'n</Button>
+                    <p className='options'>
+                        <Link to='/signup'>Sign Up</Link> or <Link to='/'>Learn More</Link>
+                    </p>
                 </form>
             </StyledContainer>
             
@@ -94,6 +87,7 @@ const StyledSection = styled.section`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    width: 100%;
 `
 
 const StyledContainer = styled.div`
@@ -101,9 +95,53 @@ const StyledContainer = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    width: 40%;
+    background: rgba(235, 244, 255, 0.95);
+
+    h1 {
+        font-size: 6.4rem;
+        margin-top: 5%;
+        margin-bottom: 4%;
+        font-weight: bold;
+    }
+
+    p {
+        font-size: 1.8rem;
+        width: 454px;
+        text-align: center;
+        margin-bottom: 5%;
+    }
 
     form {
         display: flex;
         flex-direction: column;
+        align-items: center;
+        width: 100%;
+
+        .MuiFormControl-root {
+            margin-bottom: 5%;
+            width: 358px;
+            background: #ffffff;
+            width: 60%;
+            box-shadow: 0px 7px 29px rgba(0, 0, 0, 0.25);
+            
+            input {
+                font-size: 2.5rem;
+            }
+        }
+
+        button {
+            background: #FCDE7B;
+            font-size: 2.8rem;
+            width: 60%;
+            margin-top: 2%;
+            margin-bottom: 3%;
+            box-shadow: 0px 7px 29px rgba(0, 0, 0, 0.25);
+        }
+    }
+
+    .options, a {
+        font-size: 2rem;
+        color: #F17012;
     }
 `
